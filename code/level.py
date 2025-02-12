c48e4b6a3340ba11a7d74baea69cd1e50b664f91
shubhu1026 <shubhu.p2000@gmail.com>
2025-02-12 01:01:40 -0500

    Importing decoration and 3d visuals

--- code/level.py
+++ code/level.py
@@ -2,7 +2,9 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic
+from sprites import Generic, Water, WildFlower, Tree
+from support import *
+from pytmx.util_pygame import load_pygame
 
 class Level:
 	def __init__(self):
@@ -17,13 +19,41 @@ class Level:
 		self.overlay = Overlay(self.player)
 
 	def setup(self):
+		tmx_data = load_pygame('data/map.tmx')
+
+		# house
+		for layer in ['HouseFloor', 'HouseFurnitureBottom']:
+			for x, y, surf in tmx_data.get_layer_by_name(layer).tiles():
+				Generic(pos=(x * TILE_SIZE, y * TILE_SIZE), surf=surf, groups=self.all_sprites, z=LAYERS['house bottom']) 
+
+		for layer in ['HouseWalls', 'HouseFurnitureTop']:
+			for x, y, surf in tmx_data.get_layer_by_name(layer).tiles():
+				Generic(pos=(x * TILE_SIZE, y * TILE_SIZE), surf=surf, groups=self.all_sprites, z=LAYERS['house bottom']) 
+
+		# Fence
+		for x, y, surf in tmx_data.get_layer_by_name('Fence').tiles():
+			Generic((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites) 
+
+		# Water
+		water_frames = import_folder('graphics/water')
+		for x, y, surf in tmx_data.get_layer_by_name('Water').tiles():
+			Water((x * TILE_SIZE, y * TILE_SIZE), water_frames, self.all_sprites)
+
+		# Trees
+		for obj in tmx_data.get_layer_by_name('Trees'):
+			Tree((obj.x, obj.y), obj.image, self.all_sprites, obj.name)
+
+		# Wild flowers
+		for obj in tmx_data.get_layer_by_name('Decoration'):
+			WildFlower((obj.x, obj.y), obj.image, self.all_sprites)
+
+		self.player = Player((640, 360), self.all_sprites)
 		Generic(
 			pos = (0,0),
 		  	surf = pygame.image.load('graphics/world/ground.png').convert_alpha(),
 			groups = self.all_sprites,
 			z = LAYERS['ground']
 		)
-		self.player = Player((640, 360), self.all_sprites)
 
 	def run(self,dt):
 		self.display_surface.fill('black')
@@ -41,10 +71,10 @@ class CameraGroup(pygame.sprite.Group):
 
 	def custom_draw(self, player):
 		self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
-		self.offset.y = player.rect.centerx - SCREEN_HEIGHT / 2
+		self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
 
 		for layer in LAYERS.values():
-			for sprite in self.sprites():
+			for sprite in sorted(self.sprites(), key= lambda sprite: sprite.rect.centery):
 				if sprite.z == layer:
 					offset_rect = sprite.rect.copy()
 					offset_rect.center -= self.offset
